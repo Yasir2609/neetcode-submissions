e8584cc968c330fbd050fe9f9d22359041dcd941
Mohammed Yasir Yusuf <yusufyasir490@gmail.com>
2026-08-04 21:59:07 +0530

    Add: validate-parentheses - submission-0

---
+++ Data Structures & Algorithms/validate-parentheses/submission-0.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> st;
+
+        if (s.length() % 2 != 0)
+            return false;
+
+        for (char c : s) {
+            if (c == '(' || c == '[' || c == '{') {
+                st.push(c);
+            } else {
+                if (st.empty())
+                    return false;
+
+                if (c == ')') {
+                    if (st.top() == '(')
+                        st.pop();
+                    else
+                        return false;
+                }
+                else if (c == ']') {
+                    if (st.top() == '[')
+                        st.pop();
+                    else
+                        return false;
+                }
+                else if (c == '}') {
+                    if (st.top() == '{')
+                        st.pop();
+                    else
+                        return false;
+                }
+                else {
+                    return false;
+                }
+            }
+        }
+
+        return st.empty();
+    }
+};
